0c58dbb9299a79f729bebf12c096df5f9adb86c2
Olavo Amorim Santos <olavo.a.santos@gmail.com>
2022-04-21 18:12:28 -0500

    feat: created service container mock

---
+++ service-container/index.ts
@@ -0,0 +1,31 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
+import type {Static} from '@micra/core/utilities/Static';
+import {fn} from '@/spy';
+import {MockEventEmitter} from '..';
+
+export class MockServiceContainer
+  extends MockEventEmitter
+  implements Micra.ServiceContainer
+{
+  register = fn();
+  singleton = fn();
+  factory = fn();
+  value = fn();
+  use = fn();
+  has = fn();
+  clone = fn();
+  /**
+   * It allows you to pass custom mocked functions which will be set to the instance.
+   *
+   * @param partial Partial of the Micra.ServiceContainer
+   * @returns Static Micra.ServiceContainer
+   */
+  static with(partial: Micra.ServiceContainer): Static<Micra.ServiceContainer> {
+    return class ExtendedMockServiceContainer extends MockServiceContainer {
+      constructor() {
+        super();
+        Object.assign(this, partial);
+      }
+    };
+  }
+}
